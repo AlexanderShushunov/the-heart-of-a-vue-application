3710aec69638c581c1d218f5ad1b2b78dbac9127
AlexanderShushunov <AShushunov@mail.ru>
2020-05-31 13:23:24 +0300

    Create an app by vue-cli

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="hello">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HelloWorld',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<style scoped>
+  h1 {
+    margin: 40px 0 0;
+  }
+</style>
